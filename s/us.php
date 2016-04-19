2aa42481dd2ac048ced6c52af9d678f6f68700aa
tiaro77 <jaro@kozacki.pl>
2016-04-19 11:41:48 +0200

    Create us.php

---
+++ s/us.php
@@ -0,0 +1,13 @@
+
+<?php
+
+for ($b = 0; $b<3000; $b++)  {
+  echo "<url>
+    <loc>http://badhub.net/all/us/$b</loc>
+    <lastmod>2016-03-16</lastmod>
+    <changefreq>hourly</changefreq>
+    <priority>1.0</priority>
+  </url>";
+ 
+}
+?>
